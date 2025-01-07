b51b3fe18090d74dbd23cd105a283a4916b09089
Timothée Jouffrieau <timothee.jouffrieau.ext@inetum.com>
2025-01-07 22:38:48 +0100

    1.1-Compteur qui ne fonctionne pas

--- app/src/main/java/com/example/movierecap/MainActivity.kt
+++ app/src/main/java/com/example/movierecap/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.movierecap.ui.theme.MovieRecapTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +28,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MovieRecapTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Compteur(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +38,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MovieRecapTheme {
-        Greeting("Android")
+fun Compteur(modifier: Modifier = Modifier) {
+    var compteur = 0
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Valeur : $compteur")
+        Button(
+            onClick = { compteur++ }
+        ) {
+            Text("+1")
+        }
     }
 }
